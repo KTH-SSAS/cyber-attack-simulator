ae83e6ef96452c3fb810599661ec0b536478589c
Pontus Johnson <zr45st@gmail.com>
2021-06-25 13:15:24 +0000

    Added http server attack path and refactoring.

--- attack_simulation_env.py
+++ attack_simulation_env.py
@@ -18,35 +18,64 @@ class AttackStep:
 class AttackGraph:
 
     def __init__(self):
+        self.reset()
+
+    def reset(self):
         self.attack_steps = {}
         self.attack_steps['capture_ftp_flag'] = AttackStep(reward=1000)
         self.attack_steps['ftp_login'] = AttackStep(children={'capture_ftp_flag'})
         self.attack_steps['dictionary_attack'] = AttackStep(ttc=100, children={'ftp_login'})
-        self.attack_steps['identify_ftp'] = AttackStep(children={'dictionary_attack'})
-        self.attack_steps['map_network'] = AttackStep(ttc=10, children={'identify_ftp'})
+        self.attack_steps['identify_ftp_server'] = AttackStep(children={'dictionary_attack'})
+
+        self.attack_steps['capture_root_flag'] = AttackStep(reward=2000)
+        self.attack_steps['capture_user_flag'] = AttackStep(reward=500)
+        self.attack_steps['capture_db_flag'] = AttackStep(reward=250)
+        self.attack_steps['escalate_to_root'] = AttackStep(ttc=20, children={'capture_root_flag'})
+        self.attack_steps['pop_shell'] = AttackStep(ttc=10, children={'escalate_to_root', 'capture_user_flag'})
+        self.attack_steps['exploit_sqli'] = AttackStep(ttc=20, children={'pop_shell', 'capture_db_flag'})
+        self.attack_steps['find_sqli'] = AttackStep(ttc=30, children={'exploit_sqli'})
+        self.attack_steps['crawl_http_server'] = AttackStep(ttc=10, children={'find_sqli'})
+        self.attack_steps['identify_http_server'] = AttackStep(children={'crawl_http_server'})
+
+        self.attack_steps['map_network'] = AttackStep(ttc=10, children={'identify_ftp_server', 'identify_http_server'})
         self.attack_steps['internet'] = AttackStep(children={'map_network'})
         self.size = len(self.attack_steps)
 
 
-
 class Attacker:
     
-    def __init__(self, attack_graph, compromised_steps):
-        self.attack_graph = attack_graph
+    def __init__(self, compromised_steps):
+        self.attack_graph = AttackGraph()
+        print(self.attack_graph)
         self.compromised_steps = compromised_steps
-        self.current_step = random.choice(list(self.attack_surface()))
+        self.choose_next_step()
         self.time_on_current_step = 0
         self.total_time = 0
 
-    def attack_surface(self):
-        return set([child for step_name in self.compromised_steps for child in self.attack_graph.attack_steps[step_name].children]) - self.compromised_steps
+    def attack_surface(self, debug=False):
+        surface =set()
+        for step_name in self.compromised_steps:
+            for child in self.attack_graph.attack_steps[step_name].children:
+                surface.add(child)
+        surface -= self.compromised_steps
+        return surface
+        # return set([child for step_name in self.compromised_steps for child in self.attack_graph.attack_steps[step_name].children]) - self.compromised_steps
+
+    def choose_next_step(self): 
+        at_surf = self.attack_surface()
+        if at_surf:
+            self.current_step = random.choice(list(at_surf))
+        else:
+            self.current_step = None
 
     def attack(self):
+        if not self.current_step:
+            return False
         if self.time_on_current_step >= self.attack_graph.attack_steps[self.current_step].ttc:
             self.compromised_steps.add(self.current_step)
             if not self.attack_surface():
                 return False
-            self.current_step = random.choice(list(self.attack_surface()))
+            self.choose_next_step()
             self.time_on_current_step = 0
         self.time_on_current_step += 1
         self.total_time += 1
@@ -78,14 +107,13 @@ class AttackSimulationEnv(gym.Env):
 
         super(AttackSimulationEnv, self).__init__()
 
-        self.attack_graph = AttackGraph()
-        self.attacker = Attacker(self.attack_graph, {'internet'})
+        self.attacker = Attacker({'internet'})
         self.ftp_is_online = True
         self.http_is_online = True
         self.provision_reward = 0
 
 
-        self.observation_space = spaces.Box(low=0, high=1, shape=(self.attack_graph.size, 1), dtype=np.float32)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(self.attacker.attack_graph.size, 1), dtype=np.float32)
         self.action_space = spaces.Tuple((spaces.Discrete(2), spaces.Discrete(2)))
 
     def step(self, action):
@@ -105,34 +133,35 @@ class AttackSimulationEnv(gym.Env):
         obs = self._next_observation()
 
         reward = self.provision_reward - self.attacker.reward()
+        if self.attacker.current_step:
+            info = {"time": self.attacker.total_time, "current_step": self.attacker.current_step, "time_on_current_step": self.attacker.time_on_current_step, "ttc_of_current_step": self.attacker.attack_graph.attack_steps[env.attacker.current_step].ttc, "ftp_is_online": self.ftp_is_online, "http_is_online": self.http_is_online}
+        else:
+            info = {"time": self.attacker.total_time, "current_step": None, "time_on_current_step": None, "ttc_of_current_step": None, "ftp_is_online": self.ftp_is_online, "http_is_online": self.http_is_online}
 
-        info = {"time": self.attacker.total_time, "current_step": self.attacker.current_step, "time_on_current_step": self.attacker.time_on_current_step, "ttc_of_current_step": self.attacker.attack_graph.attack_steps[env.attacker.current_step].ttc, "ftp_is_online": self.ftp_is_online, "http_is_online": self.http_is_online}
-        
+
         return obs, reward, attacker_done or defender_done, info
 
     def reset(self):
-        self.attack_graph = AttackGraph()
+        self.attacker.attack_graph.reset()
         return self._next_observation()
 
     def _next_observation(self):
-        return np.array([self.attacker.observe(a) for a in self.attack_graph.attack_steps])
+        return np.array([self.attacker.observe(a) for a in self.attacker.attack_graph.attack_steps])
 
     def render(self, mode='human'):
         pass
 
     def isolate_ftp(self):
         self.ftp_is_online = False
-        self.attacker.compromised_steps -= {'identify_ftp', 'dictionary_attack', 'ftp_login', 'capture_ftp_flag', 'map_network'}
-        self.attacker.current_step = "map_network"
+        self.attacker.compromised_steps -= {'identify_ftp_server', 'dictionary_attack', 'ftp_login', 'capture_ftp_flag'}
+        self.attacker.attack_graph.attack_steps['map_network'].children -= {'identify_ftp_server'}
+        self.attacker.choose_next_step()
 
     def isolate_http(self):
         self.http_is_online = False
-
-    def reinstate_ftp(self):
-        self.ftp_is_online = True
-
-    def reinstate_http(self):
-        self.http_is_online = True
+        self.attacker.compromised_steps -= {'capture_root_flag', 'capture_user_flag', 'capture_db_flag', 'escalate_to_root', 'pop_shell', 'exploit_sqli', 'find_sqli', 'crawl_http_server', 'identify_http_server'}
+        self.attacker.attack_graph.attack_steps['map_network'].children -= {'identify_http_server'}
+        self.attacker.choose_next_step()
 
 
 env = AttackSimulationEnv()
@@ -150,5 +179,5 @@ while not done:
         online_status_changed = True
     obs, reward, done, info = env.step((ftp_online, http_online))
     if info["time_on_current_step"] == 1 or online_status_changed:
-        print("obs: " + str(obs) + " reward: " + str(reward) + " info: " + str(info))
-print("Final: obs: " + str(obs) + " reward: " + str(reward) + " info: " + str(info))
+        print("info: " + str(info) + " reward: " + str(reward))
+print("Final: info: " + str(info) + " obs: " + str(obs) + " reward: " + str(reward))
