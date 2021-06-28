1afd5e5ea0c0f1188887529bd395cce654ac1d12
Jakob Nyberg <jaknyb@kth.se>
2021-06-28 15:46:29 +0200

    q_learning agent

--- attack_simulator/agents.py
+++ attack_simulator/agents.py
@@ -30,22 +30,47 @@ class QLearningAgent(Agent):
 
 	def __init__(self, num_actions) -> None:
 		
-		pass
+		self.num_actions = num_actions
+		self.Q = {} # A dict is used to save on memory
+		self.epsilon = 0.1
+		self.alpha = 0.5
+		self.gamma = 0.5
+		self.state = None
+		self.action = None
 
-	def update(self):
-		pass
+	def act(self, state):
+		self.state = state
+		if random.rand() > self.epsilon:
+			a = self.Q.get(state)
+			if a is None:
+				a = [10]*self.num_actions
+				self.Q[state] = a
+			action = argmax(a)
+		else:
+			action = random.randint(self.num_actions)	
+
+		self.action = action
+		return action
+
+	def update(self, new_state, reward):
+		q_prime = self.Q.get(new_state)
+		if q_prime is None:
+			q_prime = [10]*self.num_actions
+			self.Q[new_state] = q_prime	
+
+		q = self.Q[self.state][self.action]
+		self.Q[self.state][self.action] = q + self.alpha*(reward + self.gamma*argmax(q_prime) - q)
 
 class RandomAgent(Agent):
 
-	def __init__(self, num_services, disable_probability):
+	def __init__(self, disable_probability):
 		self.disable_probability = disable_probability
-		self.num_services = num_services
 
 	def act(self, enabled_services):
 		for service in enabled_services:
 			if enabled_services[service] == 1 and random.uniform(0,1) < self.disable_probability:
 				enabled_services[service] = 0
-		return enabled_services
+		return None
 
 	def update(self, action, reward):
 		return
