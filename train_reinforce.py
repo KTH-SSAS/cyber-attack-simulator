952cd5d12e8924638f06ef90ee3f89bd8ec2375f
Pontus Johnson <zr45st@gmail.com>
2021-07-06 15:43:43 +0000

    Command line argument for generation of GraphViz .dot file.

--- train_reinforce.py
+++ train_reinforce.py
@@ -10,6 +10,7 @@ import argparse
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='Reinforcement learning of a computer network defender.')
+	parser.add_argument('-g', '--graph', action='store_true', help='Generate a GraphViz .dot file.')
 	parser.add_argument('-t', '--test', action='store_true', help='Run tests.')
 	parser.add_argument('-s', '--simulations', type=int, default=100, help='Number of simulations.')
 	parser.add_argument('-f', '--flag_rewards', type=int, default=1000, help='Flag rewards for the attacker (use positive values).')
@@ -22,6 +23,8 @@ if __name__ == '__main__':
 	logging.getLogger("trainer").setLevel(logging.DEBUG)
 	logging.getLogger("trainer").addHandler(logging.FileHandler("trainer.log", mode="w"))
 	env = AttackSimulationEnv(deterministic=False, flag_reward=args.flag_rewards)
+	if args.graph:
+		env.attack_graph.generate_graphviz_file()
 	attack_steps = 78
 	services = 17
 	agent = ReinforceAgent(attack_steps, services, hidden_dim=64)
