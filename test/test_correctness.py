85bf0baf331a7ba503a9a3c2dc1f7eff599ab6c2
Pontus Johnson <zr45st@gmail.com>
2021-07-06 15:44:34 +0000

    Added two more correctness tests.

--- test/test_correctness.py
+++ test/test_correctness.py
@@ -1,11 +1,8 @@
 import logging
 from attack_simulator.attack_simulation_env import AttackSimulationEnv
 
-def test(env, agent):
-	# Testing a specific state action. If 'lazarus.ftp.login', 'lazarus.ftp.dictionary_attack', 'lazarus.ftp.connect', 'office_network.map', 'office_network.connect' and 'internet.connect' are compromised, then the best action must be to disable 'lazarus'.
+def test_state_action_response(env, agent, compromised_steps, correct_action):
 	log = logging.getLogger("trainer")
-	compromised_steps = ['lazarus.ftp.login', 'lazarus.ftp.dictionary_attack', 'lazarus.ftp.connect', 'office_network.map', 'office_network.connect', 'internet.connect']
-	correct_action = 'lazarus'
 	state = env.observation_from_compromised_steps(compromised_steps)
 	action = env.interpret_action(agent.act(state))
 	if action == correct_action: 
@@ -13,3 +10,19 @@ def test(env, agent):
 	else:   
 		log.debug(f"Test failed: Selected action {action} instead of {correct_action}.")
 
+
+def test(env, agent):
+	# Testing a specific state action. If 'lazarus.ftp.login', 'lazarus.ftp.dictionary_attack', 'lazarus.ftp.connect', 'office_network.map', 'office_network.connect' and 'internet.connect' are compromised, then the best action must be to disable 'lazarus'.
+	compromised_steps = []
+	correct_actions = []
+	compromised_steps.append(['lazarus.ftp.login', 'lazarus.ftp.dictionary_attack', 'lazarus.ftp.connect', 'office_network.map', 'office_network.connect', 'internet.connect'])
+	correct_actions.append('lazarus')
+
+	compromised_steps.append(['lazarus.terminal_access', 'lazarus.tomcat.exploit_vulnerability', 'lazarus.tomcat.find_vulnerability', 'lazarus.tomcat.dictionary_attack', 'lazarus.tomcat.gather_information', 'lazarus.tomcat.connect', 'office_network.map', 'office_network.connect', 'internet.connect'])
+	correct_actions.append('lazarus')
+
+	compromised_steps.append(['energetic_bear.escalate_to_root', 'energetic_bear.exploit_vulnerability', 'energetic_bear.find_vulnerability', 'energetic_bear.terminal_access', 'energetic_bear.apache.exploit_vulnerability', 'energetic_bear.apache.find_vulnerability', 'energetic_bear.apache.gather_information', 'energetic_bear.apache.connect', 'office_network.map', 'office_network.connect', 'internet.connect'])
+	correct_actions.append('energetic_bear')
+
+	for i in range(0,len(compromised_steps)):
+		test_state_action_response(env, agent, compromised_steps[i], correct_actions[i])
