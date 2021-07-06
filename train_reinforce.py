78e0d56e166892f1675c5aee2717146fafba6295
Pontus Johnson <zr45st@gmail.com>
2021-07-06 12:16:44 +0000

    More command line arguments.

--- train_reinforce.py
+++ train_reinforce.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='Reinforcement learning of a computer network defender.')
 	parser.add_argument('-t', '--test', action='store_true', help='Run tests.')
+	parser.add_argument('-s', '--simulations', type=int, default=100, help='Number of simulations.')
+	parser.add_argument('-f', '--flag_rewards', type=int, default=1000, help='Flag rewards for the attacker (use positive values).')
 	args = parser.parse_args()
 
 	random.seed(0)
@@ -19,14 +21,12 @@ if __name__ == '__main__':
 	logging.getLogger("simulator").addHandler(logging.FileHandler("simulator.log", mode="w"))
 	logging.getLogger("trainer").setLevel(logging.DEBUG)
 	logging.getLogger("trainer").addHandler(logging.FileHandler("trainer.log", mode="w"))
-	env = AttackSimulationEnv(deterministic=False, flag_reward=100000)
+	env = AttackSimulationEnv(deterministic=False, flag_reward=args.flag_rewards)
 	attack_steps = 78
 	services = 17
 	agent = ReinforceAgent(attack_steps, services, hidden_dim=64)
 
+	run_multiple_simulations(args.simulations, env, agent)
 	if args.test:
-		run_multiple_simulations(10, env, agent)
 		test(env, agent)
-	else:
-		run_multiple_simulations(100000, env, agent)
 
