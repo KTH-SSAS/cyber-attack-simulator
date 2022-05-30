9092ae1405309b80a078f808d521bda7bff2e811
Jakob Nyberg <jakobnnyberg@gmail.com>
2022-05-30 15:15:59 +0000

    telegram notifier

---
+++ src/attack_simulator/telegram_utils.py
@@ -0,0 +1,10 @@
+import json
+import telegram
+
+def notify_ending(message):
+	with open('./telegram_keys.json', 'r') as keys_file:
+		k = json.load(keys_file)
+		token = k['telegram_token']
+		chat_id = k['chat_id']
+		bot = telegram.Bot(token=token)
+		bot.sendMessage(chat_id=chat_id, text=message)
