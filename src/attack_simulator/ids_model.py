f86727181d0dc1b764b4173e4c7cd84fc658a8a1
Jakob Nyberg <jaknyb@kth.se>
2022-07-05 10:00:04 +0200

    tweaks to model

--- src/attack_simulator/ids_model.py
+++ src/attack_simulator/ids_model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from torch import Tensor, nn
+from ray.rllib.utils.torch_utils import FLOAT_MIN, FLOAT_MAX
 
 
 def register_rllib_model():
@@ -25,7 +26,6 @@ class DefenderModel(TorchModelV2, nn.Module):
         self.action_space = action_space
         self.num_outputs = num_outputs
 
-        self.num_defense_steps = kwargs["num_defense_steps"]
 
         self.policy_fn = nn.Linear(sim_space.shape[0], self.action_space.n)
         self.value_fn = nn.Linear(sim_space.shape[0], 1)
@@ -45,7 +45,7 @@ class DefenderModel(TorchModelV2, nn.Module):
         value_out = self.value_fn(sim_state)
         self._value_out = value_out
 
-        inf_mask = torch.max(torch.log(action_mask), torch.ones_like(action_mask) * -1e10)
+        inf_mask = torch.clamp(torch.log(action_mask), FLOAT_MIN, FLOAT_MAX)
 
         return policy_out + inf_mask, state
 
