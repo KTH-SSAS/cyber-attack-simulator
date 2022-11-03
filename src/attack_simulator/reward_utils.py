cad4beaff5ff70305e4d5d7c6868c4d2c428138f
Jakob Nyberg <jakobnnyberg@gmail.com>
2022-11-03 07:42:25 +0000

    change param name

--- src/attack_simulator/reward_utils.py
+++ src/attack_simulator/reward_utils.py
@@ -65,8 +65,8 @@ def get_normalized_attacker_reward(
 
 
 def get_minimum_rewards(
-    action_cost: FLOAT_TYPE, num_actions: INT_TYPE, length: INT_TYPE
+    action_cost: FLOAT_TYPE, num_actions: INT_TYPE, episode_length: INT_TYPE
 ) -> NDArray[FLOAT_TYPE]:
     return np.array(
-        [action_cost_for_timestep(t + 1, action_cost, num_actions) for t in np.arange(length)]
+        [action_cost_for_timestep(t + 1, action_cost, num_actions) for t in np.arange(episode_length)]
     )
