649425198b38d004b0a793c6585b1f81705e49c8
Jakob Nyberg <jaknyb@kth.se>
2022-12-05 12:41:37 +0100

    tweak script

--- scripts/render_graph.py
+++ scripts/render_graph.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+import sys
 from attack_simulator.config import GraphConfig
 from attack_simulator.graph import AttackGraph
 
 seed = 22
-filename = "graphs/four_ways.yaml"
-graph_root = "attacker:13:enter:13"
+filename = sys.argv[1]
 
-config = GraphConfig(filename, graph_root, rewards={"default": 10}, ttc={"default": 1})
+config = GraphConfig(filename, rewards={"default": 10}, ttc={"default": 1})
+
 graph = AttackGraph(config)
-graph.draw()
+
+graph.draw(add_defense=True)
