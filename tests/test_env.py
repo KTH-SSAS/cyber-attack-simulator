d1c983313416bb884b8fd045a88f2deb02271ea9
Jakob Nyberg <jaknyb@kth.se>
2022-12-16 09:38:48 +0100

    move code from env to sim test

--- tests/test_env.py
+++ tests/test_env.py
@@ -10,7 +10,7 @@ from attack_simulator.renderer import AttackSimulationRenderer
 from attack_simulator.sim import AttackSimulator
 
 
-def test_env_spaces(env, attack_graph: AttackGraph):
+def test_env_spaces(env: AttackSimulationEnv, attack_graph: AttackGraph) -> None:
     num_defenses = attack_graph.num_defenses
     num_actions = env.action_space.n
     assert num_actions == num_defenses + 1
@@ -19,78 +19,19 @@ def test_env_spaces(env, attack_graph: AttackGraph):
     assert dim_observations == num_defenses + num_attacks
 
 
-def test_env_seed(env: AttackSimulationEnv):
+def test_env_seed(env: AttackSimulationEnv) -> None:
     assert env.seed() is not None
     assert [42] == env.seed()
 
 
-def test_env_reset(env: AttackSimulationEnv):
+def test_env_reset(env: AttackSimulationEnv) -> None:
     obs = np.array(env.reset())
     assert all(env.sim.observe() == obs)
 
 
-def test_env_first_observation(simulator: AttackSimulator):
-    obs = np.array(simulator.observe())
-    assert all(obs == [1] * simulator.g.num_defenses + [0] * simulator.g.num_attacks)
-
-
-def test_sim_noisy_observation(simulator: AttackSimulator):
-
-    # Set observations to be very noisy
-    simulator.false_negative = 0.5
-    simulator.false_positive = 0.5
-
-    obs1 = simulator.observe()
-    obs2 = simulator.observe()
-
-    # If not time passes, the observations do not change
-    assert all(obs1 == obs2)
-
-    # When time changes, so will the observation
-    simulator.step()
-
-    obs2 = simulator.observe()
-
-    assert any(obs1 != obs2)
-
-
-def test_env_interpretations(simulator: AttackSimulator):
-    obs = simulator.observe()
-    i_obs = simulator.interpret_observation(obs)
-    i_def = simulator.interpret_defenses(simulator.defense_state)
-    i_att = simulator.interpret_attacks(simulator.attack_state)
-    assert i_obs == (i_def, i_att)
-
-
-def test_env_action(simulator: AttackSimulator, test_defense_steps):
-    assert simulator.interpret_action(0) == simulator.NO_ACTION_STR
-    for i, service in enumerate(test_defense_steps):
-        assert simulator.interpret_action(i + 1) == service
-
-
-def test_env_action_probs(env: AttackSimulationEnv, test_defense_steps):
-    probs = [1] * env.num_actions
-    i_probs = env.interpret_action_probabilities(probs)
-    assert all(map(lambda v: v == 1, i_probs.values()))
-    assert list(i_probs) == [env.NO_ACTION] + test_defense_steps
-
-
-def test_env_render(env: AttackSimulationEnv, tmpdir):
-    with tmpdir.as_cwd():
-        assert env.render() is True
-
-
-def test_env_default_config(env_config):
-    env = AttackSimulationEnv(env_config)
-    # defaults for project graph
-    num_defenses = 2
-    num_attacks = 6
-    assert all(np.array(env.reset()) == [1] * num_defenses + [0] * num_attacks)
-
-
 @pytest.mark.parametrize("save_graphs", [False, True])
 @pytest.mark.parametrize("save_logs", [False, True])
-def test_env_render_save_graphs(save_graphs, save_logs, env_config, tmpdir):
+def test_env_render_save_graphs(save_graphs, save_logs, env_config, tmpdir) -> None:
     config: EnvConfig = dataclasses.replace(
         env_config, save_graphs=save_graphs, save_logs=save_logs
     )
