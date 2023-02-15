19ac30c727b4af4934b5e0929fbc72fabda77c1f
Jakob Nyberg <jaknyb@kth.se>
2023-02-15 07:49:29 +0100

    add parameters

--- scripts/render_graph.py
+++ scripts/render_graph.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-import sys
 from attack_simulator.config import GraphConfig
 from attack_simulator.graph import AttackGraph
 
 seed = 22
-filename = sys.argv[1]
+filename = "mgg.yaml"  # sys.argv[1]
 
 config = GraphConfig(filename, rewards={"default": 10}, ttc={"default": 1})
 
 graph = AttackGraph(config)
 
-graph.draw(add_defense=True)
+graph.draw(add_defense=True, width=300, height=400)
