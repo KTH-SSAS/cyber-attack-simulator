e788a5134e68c67e68797a2c2addf3bf616d48c2
Jakob Nyberg <jaknyb@kth.se>
2023-04-13 10:56:01 +0200

    can run some form of gnn

--- attack_simulator/models/gnn.py
+++ attack_simulator/models/gnn.py
@@ -6,6 +6,7 @@ from torch_geometric.utils import add_self_loops, degree
 from torch_geometric.data import Data, Batch
 import einops
 
+
 class GCNConv(MessagePassing):
     def __init__(self, in_channels, out_channels):
         super().__init__(aggr="add")  # "Add" aggregation (Step 5).
@@ -56,8 +57,11 @@ class GNNRLAgent(nn.Module):
         channels_out = 8
         self.embedding_func = GCNConv(1, channels_out)
 
-        self.policy_fn = nn.Linear(channels_out, num_outputs)
+        self.policy_fn = nn.Linear(channels_out, 1)
         self.value_fn = nn.Linear(channels_out, 1)
+
+        self.wait_embedding = nn.Embedding(1, channels_out)
+        self.terminate_embedding = nn.Embedding(1, channels_out)
 
     def forward(self, x, edge_index, defense_indices):
         # x has shape [B, N, in_channels]
@@ -68,7 +72,7 @@ class GNNRLAgent(nn.Module):
         # B is the batch size
         # E is the number of edges
         batch = construct_gnn_batch(x, edge_index)
-        
+
         # embedding has shape [BxN, channels_out]
         embedding = self.embedding_func(batch.x, batch.edge_index)
 
@@ -76,21 +80,28 @@ class GNNRLAgent(nn.Module):
         # This assumes that all graphs in the batch have the same number of nodes
         # https://einops.rocks/1-einops-basics/#decomposition-of-axis
         embedding = einops.rearrange(embedding, "(b n) c -> b n c", b=batch.num_graphs)
-        
+
+        # gather embeddings for defense step
         defense_indices = einops.repeat(defense_indices, "b k -> b k d", d=embedding.shape[-1])
-        
         defense_embeddings = torch.gather(embedding, 1, defense_indices)
-        
-        # Take the mean of the node embeddings to get a graph embedding
-        # embedding = torch.mean(embedding, dim=1)
+
+        # add embeddings for wait and terminate actions
+        wait_repeated = einops.repeat(
+            self.wait_embedding.weight, "k d -> b k d", b=embedding.shape[0]
+        )
+        terminate_repeated = einops.repeat(
+            self.terminate_embedding.weight, "k d -> b k d", b=embedding.shape[0]
+        )
+        policy_in = torch.cat([defense_embeddings, wait_repeated, terminate_repeated], dim=1)
 
         # Compute policy and value outputs
         # policy_out has shape [B, num_outputs]
         # only compute policy func for defense nodes
-        policy_out = self.policy_fn(defense_embeddings)
-        
+        policy_out = self.policy_fn(policy_in).squeeze()
+
+        # Take the mean of the node embeddings to get a (rough) graph embedding
         # value_out has shape [B, 1]
-        value_out = self.value_fn(defense_embeddings)
+        value_out = self.value_fn(embedding.mean(dim=1)).squeeze()
 
         return policy_out, value_out
 
