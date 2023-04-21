9e0d9e1ff98aebd137a734af0004ce27fd67f9b5
Jakob Nyberg <jaknyb@kth.se>
2023-04-21 07:30:22 +0200

    update defenderconfig

--- attack_simulator/rllib/ids_model.py
+++ attack_simulator/rllib/ids_model.py
@@ -33,10 +33,28 @@ class DefenderConfig(PPOConfig):
         # self._allow_unknown_subkeys = True
 
         self.scale_rewards = True
+        self.num_special_actions = 0
+        self.wait_action = 0
+        self.terminate_action = 1
 
-    def training(self, *, scale_rewards: Optional[bool] = NotProvided, **kwargs):
+    def training(
+        self,
+        *,
+        scale_rewards: Optional[bool] = NotProvided,
+        num_special_actions: Optional[int] = NotProvided,
+        wait_action: Optional[int] = NotProvided,
+        terminate_action: Optional[int] = NotProvided,
+        **kwargs,
+    ):
         super().training(**kwargs)
-        self.scale_rewards = scale_rewards
+        self.scale_rewards = self.scale_rewards if scale_rewards is NotProvided else scale_rewards
+        self.num_special_actions = (
+            self.num_special_actions if num_special_actions is NotProvided else num_special_actions
+        )
+        self.wait_action = self.wait_action if wait_action is NotProvided else wait_action
+        self.terminate_action = (
+            self.terminate_action if terminate_action is NotProvided else terminate_action
+        )
         return self
 
     def update_from_dict(self, config_dict: PartialAlgorithmConfigDict) -> DefenderConfig:
