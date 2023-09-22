3488ad0e2945a77f36067471da8baed2da4a50d4
Jakob Nyberg <jaknyb@kth.se>
2023-09-22 09:38:20 +0200

    hierarchical MLP agent

--- attack_simulator/models/dense.py
+++ attack_simulator/models/dense.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch
 
 
 class HiddenLayer(nn.Module):
@@ -54,3 +55,30 @@ class MLPRLAgent(nn.Module):
             value_out = self.value_fn(self.vf_embedding_func(x))
 
         return policy_out, value_out
+
+class HierarchicalMLPRLAgent(nn.Module):
+    def __init__(self, input_size, mlp_hidden_dims, num_a0_actions, num_a1_actions, vf_share_layers) -> None:
+        super().__init__()
+        self.embedding_func = MLP(input_size, mlp_hidden_dims)
+
+        if not vf_share_layers:
+            self.vf_embedding_func = MLP(input_size, mlp_hidden_dims)
+        else:
+            self.vf_embedding_func = self.embedding_func
+
+        self.a0_policy_fn = nn.Linear(mlp_hidden_dims[-1], num_a0_actions)
+        self.a1_policy_fn = nn.Linear(mlp_hidden_dims[-1] + num_a0_actions, num_a1_actions)
+        self.value_fn = nn.Linear(mlp_hidden_dims[-1], 1)
+        self.vf_share_layers = vf_share_layers
+
+    def forward(self, x):
+        embedding = self.embedding_func(x)
+        a0_out = self.a0_policy_fn(embedding)
+        a1_out = self.a1_policy_fn(torch.cat([embedding, a0_out], dim=-1))
+
+        if self.vf_share_layers:
+            value_out = self.value_fn(embedding)
+        else:
+            value_out = self.value_fn(self.vf_embedding_func(x))
+
+        return torch.cat([a0_out, a1_out], dim=-1), value_out
