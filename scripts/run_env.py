f7f953d7c614ea224583eb87328e68495888875a
Jakob Nyberg <jaknyb@kth.se>
2023-09-29 10:26:14 +0200

    update run script

--- scripts/run_env.py
+++ scripts/run_env.py
@@ -1,3 +1,4 @@
+import pygame
 import torch
 from attack_simulator.constants import AGENT_ATTACKER, AGENT_DEFENDER
 from attack_simulator.agents.attackers.searchers import BreadthFirstAttacker
@@ -13,17 +14,30 @@ class KeyboardAgent:
         self.vocab = vocab
 
     def compute_action_from_dict(self, obs):
-        
-        nodes = obs["state"]
-        available_actions = np.flatnonzero(obs["attack_surface"])
-        action_strings = [self.vocab[a] for a in available_actions]
+        assets = obs["asset"]
+        asset_ids = obs["asset_id"]
+        step_names = obs["step_name"]
+        available_actions = np.flatnonzero(obs["node_surface"])
+        assets = [self.vocab[i] for i in assets[available_actions]]
+        asset_ids = asset_ids[available_actions]
+        step_names = [self.vocab[i] for i in step_names[available_actions]]
+        action_strings = [f"{a}:{i}:{s}" for a, i, s in zip(assets, asset_ids, step_names)]
         action_strings = [f"{i}. {a}" for i, a in enumerate(action_strings)]
         print("Available actions:")
         print("\n".join(action_strings))
 
-        print("Enter action:")
-        node = int(input())
-        return (1, node)
+        node = -1
+        while node >= len(available_actions) or node < 0:
+            print("Enter action:")
+            node = int(input())
+            if node >= len(available_actions) or node < 0:
+                print("Invalid action.")
+
+        print(f"Selected action: {action_strings[node]}")
+
+        print(available_actions[node])
+
+        return (1, available_actions[node])
 
 
 env_config = attack_simulator.EnvConfig.from_yaml("config/maze_env_config.yaml")
@@ -36,7 +50,8 @@ hidden_size = 4
 #q_range = (-100.0, 200.0 * env.n_nodes)
 defender = Net()
 #target_net = Net(q_range)
-attacker = BreadthFirstAttacker({})#KeyboardAgent(env.simulator.reverse_vocab)
+#attacker = BreadthFirstAttacker({})
+attacker = KeyboardAgent(env.reverse_vocab)
 
 
 obs, info = env.reset()
@@ -72,6 +87,7 @@ with torch.no_grad():
         #        plt.draw()
         #        plt.pause(0.01)
 
+        env.render()
         node_feats, edge_attr, edge_index = (
             obs["defender"]["ids_observation"].reshape(-1, 1),
             None,
@@ -80,7 +96,7 @@ with torch.no_grad():
 
         s_batch = [(node_feats, edge_attr, edge_index)]
         defender_action_mask = obs["defender"]["action_mask"]
-        defender_node_mask = obs["defender"]["defense_surface"]
+        defender_node_mask = obs["defender"]["node_surface"]
         defender_action = defender(s_batch, defender_action_mask, defender_node_mask)
         node_selection, value, action_probs, node_probs = defender_action
         action = 0 if len(node_selection[0]) == 0 else 1
@@ -89,8 +105,8 @@ with torch.no_grad():
         action_dict = {AGENT_ATTACKER: attacker_action, AGENT_DEFENDER: (action, node_selection)}
         if action == 1:
             assert defender_node_mask[node_selection] == 1
-        env.render()
-        input()
+        print("Press Enter to continue.")
+        #input()
         obs, rewards, terminated, truncated, infos = env.step(action_dict)
 
         # dists = {AGENT_DEFENDER: defender_action_dist.numpy().tolist()}
@@ -108,5 +124,10 @@ with torch.no_grad():
         }
 
         obs_log.write(f"{json.dumps(log, cls=NumpyArrayEncoder)}\n")
+
+env.render()
+print("Done. Press Enter to exit.")
+input()
+env.close()
 
 obs_log.close()
