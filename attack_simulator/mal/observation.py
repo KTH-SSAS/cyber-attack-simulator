ab24b5d1c2e52fd4c5fc69fd2842106b7ceb6703
Jakob Nyberg <jaknyb@kth.se>
2023-10-12 14:30:54 +0200

    update some field names

--- attack_simulator/mal/observation.py
+++ attack_simulator/mal/observation.py
@@ -27,16 +27,16 @@ class Observation:
     def from_rust(cls, obs):
         assets, asset_ids, names = zip(*obs.step_info)
         return Observation(
-            ids_observation=np.array(obs.observation, dtype=np.int8),
-            attack_surface=np.array(obs.attack_surface, dtype=np.int8),
-            defense_surface=np.array(obs.defense_surface, dtype=np.int8),
+            ids_observation=np.array(obs.defender_observation, dtype=np.int8),
+            attack_surface=np.array(obs.attacker_possible_objects, dtype=np.int8),
+            defense_surface=np.array(obs.defender_possible_objects, dtype=np.int8),
             state=np.array(obs.state, dtype=np.int8),
             assets=np.array(assets, dtype=np.int64),
             asset_ids=np.array(asset_ids, dtype=np.int64),
             names=np.array(names, dtype=np.int64),
             ttc_remaining=np.array(obs.ttc_remaining, dtype=np.int64),
-            defender_action_mask=np.array(obs.defender_action_mask, dtype=np.int8),
-            attacker_action_mask=np.array(obs.attacker_action_mask, dtype=np.int8),
+            defender_action_mask=np.array(obs.defender_possible_actions, dtype=np.int8),
+            attacker_action_mask=np.array(obs.attacker_possible_actions, dtype=np.int8),
             edges=np.array(obs.edges, dtype=np.int64),
             flags=np.array(obs.flags, dtype=np.int64),
             attacker_reward=obs.attacker_reward,
