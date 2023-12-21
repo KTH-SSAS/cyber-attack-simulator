a78434dc1406ea12f804d27d16242ed094baf448
Jakob Nyberg <jaknyb@kth.se>
2023-12-21 13:01:34 +0100

    cleanup

--- crate/kth_main.py
+++ crate/kth_main.py
@@ -3,16 +3,16 @@ import json
 from typing import Dict, Tuple, Any
 import torch
 from torch import nn
-from ray import rllib
 from typing import Union
 from crate.foi_wazuh_lib import query_wazuh, send_action_to_wazuh
-from crate.mapping import convert_to_wazuh_action, convert_wazuh_response_to_obs
-import crate.mapping
+from crate.kth_mapping import convert_to_wazuh_action, convert_wazuh_response_to_obs
+import crate.kth_mapping
 wazuh_hostname = "localhost"
 poll_time = 10
 
-num_hosts = crate.mapping.num_hosts
-num_actions = crate.mapping.num_actions
+num_hosts = crate.kth_mapping.num_hosts
+num_actions = crate.kth_mapping.num_actions
+
 
 async def step(action: Union[int, tuple]) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     """
@@ -44,10 +44,8 @@ async def main() -> None:
             obs, info = await step(action)
 
 
-# Save an untrained model for testing
-trainer = rllib.agents.ppo.PPOTrainer(
-    env = "AttackEnv",
-)
+# Save an untrained model for testing purposes
+model = nn.Linear(num_hosts, num_actions)
 
 with open("model.pt", "wb") as f:
     torch.save(model, f)
